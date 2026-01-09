fdced0146ec0c5aed9ed892c248533eb63390c4e
Amia33 <137411471+Amia33@users.noreply.github.com>
2026-01-09 15:11:07 +0800

    Initialize

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://astro.amia.work",
+  output: "server",
+  adapter: cloudflare({ imageService: "cloudflare" }),
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
